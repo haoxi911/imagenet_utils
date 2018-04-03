e36f8b21776af5a5f7dc39a655317e73504cbdcf
Hao Xi <kevin@picturekeeper.com>
2018-04-03 20:54:39 +0800

    Update script.

--- obtain_hyponym_of_synset.py
+++ obtain_hyponym_of_synset.py
@@ -1,44 +1,55 @@
 import os
 import tarfile
-import datetime
 import shutil
 import sys
-import urllib2
 import csv
 
-BASE_URL = "http://www.image-net.org/api/text/wordnet.structure.hyponym?wnid=%s"
 
-def _read_synset_csv():
-    with open('synsets.csv', mode='r') as infile:
-        reader = csv.reader(infile)
+def _read_synset_words():
+    with open('words.txt', mode='r') as infile:
+        reader = csv.reader(infile, delimiter='\t')
         return {rows[0]: rows[1] for rows in reader}
 
 
-def _list_hyponym(dict, wnid, output={}):
-    output[wnid] = dict[wnid]
-    contents = urllib2.urlopen(BASE_URL % wnid ).read()
-    lines = contents.splitlines()
-    for line in lines:
-        if line.startswith('-'):
-            wnid = line[1:]
-            output = _list_hyponym(dict, wnid, output)
+def _read_synset_nodes():
+    dic = {}
+    with open('wordnet.is_a.txt', mode='r') as infile:
+        reader = csv.reader(infile, delimiter=' ')
+        for rows in reader:
+            dic.setdefault(rows[0], []).append(rows[1])
+    return dic
+
+
+def _list_hyponym(wnid):
+    output = {}
+    _list_hyponym_sub(
+        wnid, _read_synset_words(), _read_synset_nodes(), output
+    )
     return output
 
 
-def _extract_tars(dataset, wnid, dict):
-    dir = os.path.join(os.getcwd(), wnid)
-    if os.path.exists(dir):
-        shutil.rmtree(dir)
-    os.makedirs(dir)
-    for key, value in dict.iteritems():
+def _list_hyponym_sub(wnid, words, nodes, output):
+    output[wnid] = words[wnid]
+    if wnid in nodes:
+        for node in nodes[wnid]:
+            _list_hyponym_sub(node, words, nodes, output)
+
+
+def _extract_tars(dataset, wnid, nodes):
+    path = os.path.join(os.getcwd(), wnid)
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    os.makedirs(path)
+    for key, value in nodes.iteritems():
         tar = os.path.join(dataset, key + '.tar')
         if not os.path.exists(tar):
             print("Tarball wasn't found at path: %s" % tar)
         else:
-            dir = os.path.join(dir, key + " " + value)
-            os.makedirs(dir)
-            tar = tarfile.open(tar)
-            tar.extractall(dir)
+            path = os.path.join(path, key + " " + value)
+            os.makedirs(path)
+            print("Extract tarball to folder: %s" % path)
+            handle = tarfile.open(tar)
+            handle.extractall(path)
 
 
 if __name__ == '__main__':
@@ -46,6 +57,5 @@ if __name__ == '__main__':
         print("Usage: obtain_hyponym_of_synset.py dataset_folder wnid")
 
     if len(sys.argv) == 3:
-        dict = _read_synset_csv()
-        dict = _list_hyponym(dict, sys.argv[2])
-        _extract_tars(sys.argv[1], sys.argv[2], dict)
+        nodes = _list_hyponym(sys.argv[2])
+        _extract_tars(sys.argv[1], sys.argv[2], nodes)
