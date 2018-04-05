b4ad00cc3163f101444cc18f3a87eea32a32d380
Hao Xi <kevin@picturekeeper.com>
2018-04-05 17:45:14 +0800

    Script to summarize all children nodes of a wnid into a csv file (without extracting any tarballs).

---
+++ summarize_hyponym_of_synset.py
@@ -0,0 +1,54 @@
+import os
+import tarfile
+import sys
+import csv
+
+
+def _read_synset_words():
+    with open('words.txt', mode='r') as infile:
+        reader = csv.reader(infile, delimiter='\t')
+        return {rows[0]: rows[1] for rows in reader}
+
+
+def _read_synset_nodes():
+    dic = {}
+    with open('wordnet.is_a.txt', mode='r') as infile:
+        reader = csv.reader(infile, delimiter=' ')
+        for rows in reader:
+            dic.setdefault(rows[0], []).append(rows[1])
+    return dic
+
+
+def _list_hyponym_sub(wnid, words, nodes, output):
+    output[wnid] = words[wnid]
+    if wnid in nodes:
+        for node in nodes[wnid]:
+            _list_hyponym_sub(node, words, nodes, output)
+
+
+def _export_summary(imagenet_folder, wnid):
+    items = {}
+    words = _read_synset_words()
+    nodes = _read_synset_nodes()
+    _list_hyponym_sub(wnid, words, nodes, items)
+
+    with open(('summary-%s.csv' % wnid), 'wb') as csv_file:
+        writer = csv.writer(csv_file)
+        for key, value in items.iteritems():
+            filepath = os.path.join(imagenet_folder, key + '.tar')
+            if not os.path.exists(filepath):
+                print("Tarball wasn't found at path: %s" % filepath)
+            else:
+                tar = tarfile.open(filepath)
+                count = len(tar.getmembers())
+                tar.close()
+                writer.writerow([key, count, value])
+                print("Found {:5d} images in tarball {:s}".format(count, filepath))
+
+
+if __name__ == '__main__':
+    if not (len(sys.argv) == 3):
+        print("Usage: obtain_hyponym_of_synset.py imagenet_folder wnid")
+
+    if len(sys.argv) == 3:
+        _export_summary(sys.argv[1], sys.argv[2])
