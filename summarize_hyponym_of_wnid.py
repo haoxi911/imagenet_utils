b6cbbfdce3ee3d75d1ce6c738ec3123d8f2b8a9e
Hao Xi <kevin@picturekeeper.com>
2018-04-19 15:46:34 +0800

    Update script. Allow us to pass multiple wnids as a batch.

--- summarize_hyponym_of_wnid.py
+++ summarize_hyponym_of_wnid.py
@@ -50,5 +50,5 @@ if __name__ == '__main__':
     if not (len(sys.argv) >= 3):
         print("Usage: summarize_hyponym_of_wnid.py imagenet_folder wnid")
     else:
-        for wnid in sys.argv[1:]:
+        for wnid in sys.argv[2:]:
             _export_summary(sys.argv[1], wnid)
